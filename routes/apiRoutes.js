9a95c5045c8d600fa065216c807c3f605e0698a3
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-07-24 11:44:19 -0700

    made changes to js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,50 +1,68 @@
-
-// dependencies 
+// dependencies
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs').promises; // Use fs.promises for async file operations
 
 // npm package that allows for unique ids to be created
-var uniqid = require('uniqid');
-
+const uniqid = require('uniqid');
 
 // routing
 module.exports = (app) => {
 
   // GET /api/notes should read the db.json file and return all saved notes as JSON.
-  app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8")
+      .then(function (data) {
+        const notes = JSON.parse(data); // No need to use [].concat() here
+        res.json(notes);
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to read notes.' });
+      });
   });
 
-  // POST /api/notes should receive a new note to save on the request body, 
-  // add it to the db.json file, and then return the new note to the client. 
-  app.post('/api/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    // creating body for note
-    let userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      // creating unique id for each note
-      id: uniqid(),
-    };
-    // pushing created note to be written in the db.json file
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
+  // POST receive a new note to save on the request body.
+  app.post('/api/notes', function (req, res) {
+    const note = req.body;
 
+    fs.readFile("./db/db.json", "utf8")
+      .then(function (data) {
+        const notes = JSON.parse(data);
+        note.id = uniqid(); // Use uniqid() to generate a unique ID
+        notes.push(note);
+        return notes;
+      })
+      .then(function (notes) {
+        return fs.writeFile("./db/db.json", JSON.stringify(notes));
+      })
+      .then(function () {
+        res.json(note);
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to save note.' });
+      });
   });
 
+  // DELETE receive a query parameter containing the id of a note to delete.
+  app.delete('/api/notes/:id', function (req, res) {
+    const idToDelete = req.params.id; // No need to parse as it's already a string
 
-  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
-  app.delete('/api/notes/:id', (req, res) => {
-    // reading notes form db.json
-    let db = JSON.parse(fs.readFileSync('db/db.json'))
-    // removing note with id
-    let deleteNotes = db.filter(item => item.id !== req.params.id);
-    // Rewriting note to db.json
-    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-    res.json(deleteNotes);
-    
-  })
+    fs.readFile("./db/db.json", "utf8")
+      .then(function (data) {
+        const notes = JSON.parse(data);
+        const newNotesData = notes.filter((note) => note.id !== idToDelete);
+        return newNotesData;
+      })
+      .then(function (notes) {
+        return fs.writeFile("./db/db.json", JSON.stringify(notes));
+      })
+      .then(function () {
+        res.send('Note deleted successfully!');
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to delete note.' });
+      });
+  });
 };
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// Dependencies
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
+const readFileAsuc = util.promisify(fs.readFile);
 // app use express
 const app = express();
-
-// creating environment variable port for  other site heroku
+// creating environment variable port for other site heroku
 const PORT = process.env.PORT || 5000;
 
-
-// asks express to create a route for every file in the 'public' folder and give it a '/' route
-app.use(express.static('public'));
-// sets up express app to handel data parser, middle wear created req.body
+// asks express to create a route
+app.use(express.static(path.join(__dirname, 'public')));
+// sets up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// routes to route files
+// routes to route files middleware
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-
 // app listener - starts the server
 app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
+  console.log(`Server available at localhost:${PORT}`);
 });
